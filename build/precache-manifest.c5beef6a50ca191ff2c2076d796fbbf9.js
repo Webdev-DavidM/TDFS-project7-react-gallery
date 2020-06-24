35ceaf8c425e926afacef8d56ccbf8f0bb648332
Webdev-DavidM <footballblubber@googlemail.com>
2020-06-24 11:40:38 +0100

    m

---
+++ build/precache-manifest.c5beef6a50ca191ff2c2076d796fbbf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243a54d72bac104b2d1192bccf668e9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0870e5525cda3123eedf",
+    "url": "/static/css/main.04fc192e.chunk.css"
+  },
+  {
+    "revision": "dd96d89d85e502b38ba2",
+    "url": "/static/js/2.004f842d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.004f842d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0870e5525cda3123eedf",
+    "url": "/static/js/main.5b09a23f.chunk.js"
+  },
+  {
+    "revision": "852b122b4ff584dcbb64",
+    "url": "/static/js/runtime-main.5f3f3636.js"
+  }
+]);
