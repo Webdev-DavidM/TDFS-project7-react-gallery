e1884f71b83d7bd1a50f599b69e208c1df2626ce
Webdev-DavidM <footballblubber@googlemail.com>
2020-11-06 09:04:57 +0000

    Updates

---
+++ precache-manifest.4fa86cc4a5b8b937d38dcae8be644508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "632c955aa953795d561600893ed7dcde",
+    "url": "/TDFS-project7-react-gallery/index.html"
+  },
+  {
+    "revision": "96fbf757a05c047491de",
+    "url": "/TDFS-project7-react-gallery/static/css/main.04fc192e.chunk.css"
+  },
+  {
+    "revision": "dd96d89d85e502b38ba2",
+    "url": "/TDFS-project7-react-gallery/static/js/2.004f842d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TDFS-project7-react-gallery/static/js/2.004f842d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96fbf757a05c047491de",
+    "url": "/TDFS-project7-react-gallery/static/js/main.ceab8be5.chunk.js"
+  },
+  {
+    "revision": "c4ffaae3f63333f92ac3",
+    "url": "/TDFS-project7-react-gallery/static/js/runtime-main.d2ebe338.js"
+  }
+]);
